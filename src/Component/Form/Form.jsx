264a301a5cd7247de5e3613505b5adfd40afa5ff
D-D-f <defreitas-david@outlook.fr>
2024-04-04 17:21:58 +0200

    add formulaire

--- src/Component/Form/Form.jsx
+++ src/Component/Form/Form.jsx
@@ -1,25 +1,95 @@
-import React from "react";
+import React, { useState } from "react";
+import { CalendarPicker } from "simple-calendar-react-library";
+import { useForm } from "react-hook-form";
 import "./Form.css";
 
 const Form = () => {
+  const [activeCalendarBirthday, setActiveCalendarBirthday] = useState(false);
+  const [activeCalendarStart, setActiveCalendarStart] = useState(false);
+  const [dateStarAndBirthday, setDateStartAndBirthday] = useState({
+    birthday: "",
+    start: "",
+  });
+  const { register, handleSubmit } = useForm();
+  const onSubmit = (d) => {
+    console.log(d);
+  };
+
+  const getDateBirthday = (date) => {
+    setDateStartAndBirthday((prev) => ({
+      ...prev,
+      birthday: date,
+    }));
+  };
+
+  const getDateStart = (date) => {
+    setDateStartAndBirthday((prev) => ({
+      ...prev,
+      start: date,
+    }));
+  };
+
+  const displayCalendarBirthday = () => {
+    setActiveCalendarBirthday(!activeCalendarBirthday);
+  };
+
+  const displayCalendarStart = () => {
+    setActiveCalendarStart(!activeCalendarStart);
+  };
+
   return (
-    <form>
-      <p className="containerInput">
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div className="containerInput">
         <label htmlFor="firstName">First Name</label>
-        <input type="text" name="firstName" />
-      </p>
-      <p className="containerInput">
+        <input type="text" {...register("firstName")} />
+      </div>
+      <div className="containerInput">
         <label htmlFor="lastName">Last Name</label>
-        <input type="text" name="lastName" />
-      </p>
-      <p className="containerInput">
+        <input type="text" {...register("lastName")} />
+      </div>
+      <div className="containerInput">
         <label htmlFor="dateofbirth">Date Of Birth</label>
-        <input type="text" name="dateofbirth" />
-      </p>
-      <p className="containerInput">
+        <input
+          type="text"
+          {...register("dateofbirth")}
+          value={dateStarAndBirthday.birthday}
+          onClick={displayCalendarBirthday}
+          readOnly
+        />
+        <div className="calendar">
+          {activeCalendarBirthday && (
+            <CalendarPicker getDate={getDateBirthday} langage="fr" />
+          )}
+        </div>
+      </div>
+      <div className="containerInput">
         <label htmlFor="startdate">Start Date</label>
-        <input type="text" name="startdate" />
-      </p>
+        <input
+          type="text"
+          {...register("startdate")}
+          value={dateStarAndBirthday.start}
+          onClick={displayCalendarStart}
+          readOnly
+        />
+        <div className="calendar">
+          {activeCalendarStart && (
+            <CalendarPicker getDate={getDateStart} langage="fr" />
+          )}
+        </div>
+      </div>
+      <fieldset>
+        <legend>Address</legend>
+        <div className="containerInput">
+          <label htmlFor="street">Street</label>
+          <input type="text" {...register("street")} />
+        </div>
+        <div className="containerInput">
+          <label htmlFor="city">City</label>
+          <input type="text" {...register("city")} />
+        </div>
+      </fieldset>
+
+      <button type="submit">Save</button>
     </form>
   );
 };
